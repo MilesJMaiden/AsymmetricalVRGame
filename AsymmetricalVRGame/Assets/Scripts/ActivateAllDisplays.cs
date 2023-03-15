243785b021153bdfee5d1e472dc8da94d9fb7a5d
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-15 10:42:00 +0000

    Two cameras for local play (unoptimised) Game is rendering 3 times.

---
+++ AsymmetricalVRGame/Assets/Scripts/ActivateAllDisplays.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateAllDisplays : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
---
+++ AsymmetricalVRGame/Assets/Scripts/DisplayCameraToImage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCameraToImage : MonoBehaviour
+{
+    public UnityEngine.UI.RawImage image;
+
+    private RenderTexture rt;
+    private Camera targetCamera;
+
+    private void Start()
+    {
+        targetCamera = GetComponent<Camera>();
+        int resWidth = Screen.width / 2;
+        int resHeight = Screen.height;
+        rt = new RenderTexture(resWidth, resHeight, 24);
+    }
+
+    private void LateUpdate()
+    {
+        targetCamera.targetTexture = rt;
+        image.texture = rt;
+
+        targetCamera.Render();
+
+        RenderTexture.active = rt;
+        targetCamera.targetTexture = null;
+        RenderTexture.active = null;
+    }
+}
